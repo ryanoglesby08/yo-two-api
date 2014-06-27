44b2246240dba221a6e57ed64c6bb7d11dbe379b
Ryan Oglesby <ryan.oglesby08@gmail.com>
2014-06-27 15:12:30 -0500

    DB schema change after removing hunts and hunters

--- db/schema.rb
+++ db/schema.rb
@@ -16,22 +16,6 @@ ActiveRecord::Schema.define(version: 20140627195808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "hunters", force: true do |t|
-    t.datetime "start_time",  null: false
-    t.datetime "finish_time"
-    t.integer  "hunt_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name",        null: false
-  end
-
-  create_table "hunts", force: true do |t|
-    t.string   "name",                       null: false
-    t.boolean  "active",     default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
